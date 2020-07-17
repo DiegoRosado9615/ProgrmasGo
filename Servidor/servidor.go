a4846356b2e70409d0c65076f1b5d851ec02bc92
LoboStark <rocadie9615@ciencias.unam.mx>
2020-07-17 18:29:06 -0500

    Intento 4

--- Servidor/servidor.go
+++ Servidor/servidor.go
@@ -6,9 +6,11 @@ Programa prueba en el cual montare un servidor en go
 import (
 	"log"
 	"net/http"
+	"github.com/diego/servidor"
 )
 
 func main() {
+	s=:server.New()
 	log.Fatal(http.ListenAndServe(":8000", nil))
 
 }
